e26774a83a147779a4ed2b8ac07a0c652aca6692
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-06-19 18:14:32 -0300

    Portfolio em desenvolvimento

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const el = document.querySelector(".text-subTitulo");
+const text = "Através de códigos, transformo ideias em realidade digital, com intuito de construir soluções web que encantam e facilitam a vida das pessoas."
+const interval = 50;
+
+function showText(el,text,interval) {
+
+    const char = text.split("").reverse();
+
+    const typer = setInterval(() => {
+        
+        if (!char.length) {
+            return clearInterval(typer);
+        }
+
+        const next = char.pop();
+
+        el.innerHTML += next;
+
+    }, interval)
+}
+
+showText(el, text, interval);
+
+
